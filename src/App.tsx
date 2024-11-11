76b641ba609fc7405ad817a1d6c2002f35ede66e
Kim-Hyunhee <tta_bi@naver.com>
2024-11-11 11:24:26 +0900

    테마 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
+import { GlobalStyle } from './style/global';
+import { ThemeProvider } from 'styled-components';
+import { light, dark } from './style/theme';
 
 function App() {
-  return <Layout children={<Home />} />;
+  return (
+    <ThemeProvider theme={light}>
+      <GlobalStyle themeName="light" />
+      <Layout>
+        <Home />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 // function App() {
-//   return (
-//     <Layout>
-//       <Home />
-//     </Layout>
-//   );
+//   return <Layout children={<Home />} />;
 // }
 
 export default App;
