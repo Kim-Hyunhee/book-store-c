f015fa8678c3d85b16c530b3dec8e91a0dd08568
Kim-Hyunhee <tta_bi@naver.com>
2024-11-12 12:54:20 +0900

    API 통신과 데이터 레이어

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,27 +2,11 @@ import { styled } from 'styled-components';
 import logo from '../../assets/images/logo.jpg';
 import { FaSignInAlt, FaRegUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
-const CATEGORY = [
-  {
-    id: null,
-    name: '전체',
-  },
-  {
-    id: 0,
-    name: '동화',
-  },
-  {
-    id: 1,
-    name: '소설',
-  },
-  {
-    id: 2,
-    name: '사회',
-  },
-];
+import { useCategory } from '../../hooks/useCategory';
 
 function Header() {
+  const { category } = useCategory();
+
   return (
     <HeaderStyle>
       <h1 className="logo">
@@ -32,14 +16,16 @@ function Header() {
       </h1>
       <nav className="category">
         <ul>
-          {CATEGORY.map((item) => (
-            <li key={item.id}>
+          {category.map((item) => (
+            <li key={item.categoryId}>
               <Link
                 to={
-                  item.id === null ? '/books' : `/books?category_id=${item.id}`
+                  item.categoryId === null
+                    ? '/books'
+                    : `/books?category_id=${item.categoryId}`
                 }
               >
-                {item.name}
+                {item.categoryName}
               </Link>
             </li>
           ))}
@@ -80,15 +66,15 @@ const HeaderStyle = styled.header`
     img {
       width: 200px;
     }
-  };
+  }
 
   .category {
-    ul{
-      display:flex;
-      gap:32px;
+    ul {
+      display: flex;
+      gap: 32px;
       li {
         a {
-          font-size: 1.5rem
+          font-size: 1.5rem;
           font-weight: 600;
           text-decoration: none;
           color: ${({ theme }) => theme.color.text};
@@ -101,15 +87,15 @@ const HeaderStyle = styled.header`
   }
 
   .auth {
-    ul{
+    ul {
       display: flex;
       gap: 16px;
       li {
-        font-size: 1rem
+        font-size: 1rem;
         font-weight: 600;
         text-decoration: none;
         display: flex;
-        align-item: center;
+        align-items: center;
         line-height: 1;
 
         svg {
