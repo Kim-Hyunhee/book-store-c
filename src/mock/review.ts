0627b233c9fdd54660efa61d2098be91313e375a
Kim-Hyunhee <tta_bi@naver.com>
2024-11-19 14:32:30 +0900

    메인-리뷰

--- src/mock/review.ts
+++ src/mock/review.ts
@@ -2,36 +2,18 @@ import { BookReviewItem } from '../models/book.model';
 import { http, HttpResponse } from 'msw';
 import { fakerKO as faker } from '@faker-js/faker';
 
+const mockReviewData: BookReviewItem[] = Array.from({ length: 8 }).map(
+  (_, index) => ({
+    id: index,
+    userName: `${faker.person.lastName()}${faker.person.firstName()}`,
+    content: faker.lorem.paragraph(),
+    createdAt: faker.date.past().toISOString(),
+    score: faker.number.int({ min: 1, max: 5 }),
+  })
+);
 export const reviewById = http.get(
   'http://localhost:9999/reviews/:bookId',
   () => {
-    // const data: BookReviewItem[] = [
-    //   {
-    //     id: 1,
-    //     userName: 'Bob',
-    //     content: '감사합니다',
-    //     createdAt: '2024-11-19',
-    //     score: 4,
-    //   },
-    //   {
-    //     id: 2,
-    //     userName: 'Bob2',
-    //     content: '감사합니다2',
-    //     createdAt: '2024-11-19',
-    //     score: 3,
-    //   },
-    // ];
-
-    const mockReviewData: BookReviewItem[] = Array.from({ length: 8 }).map(
-      (_, index) => ({
-        id: index,
-        userName: `${faker.person.lastName()}${faker.person.firstName()}`,
-        content: faker.lorem.paragraph(),
-        createdAt: faker.date.past().toISOString(),
-        score: faker.number.int({ min: 1, max: 5 }),
-      })
-    );
-
     return HttpResponse.json(mockReviewData, {
       status: 200,
     });
@@ -47,3 +29,7 @@ export const addReview = http.post(
     );
   }
 );
+
+export const reviewForMain = http.get('http://localhost:9999/reviews', () => {
+  return HttpResponse.json(mockReviewData, { status: 200 });
+});
