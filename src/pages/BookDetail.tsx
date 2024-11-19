63f42b5b65226dd37e8acea41e669f9716242292
Kim-Hyunhee <tta_bi@naver.com>
2024-11-19 11:00:41 +0900

    리뷰 목록

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -9,6 +9,7 @@ import { Link } from 'react-router-dom';
 import EllipsisBox from '../components/common/EllipsisBox';
 import LikeButton from '../components/book/LikeButton';
 import AddToCart from '../components/book/AddToCart';
+import BookReview from '../components/book/BookReview';
 
 const bookInfoList = [
   {
@@ -85,7 +86,13 @@ function BookDetail() {
       </header>
       <div className="content">
         <Title size="medium">상세 설명</Title>
-        <EllipsisBox linelimit={4}>{book.contents}</EllipsisBox>
+        <EllipsisBox linelimit={4}>{book.detail}</EllipsisBox>
+
+        <Title size="medium">목차</Title>
+        <p className="index">{book.contents}</p>
+
+        <Title size="medium">리뷰</Title>
+        <BookReview reviews={reviews} />
       </div>
     </BookDetailStyle>
   );
